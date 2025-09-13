138f2780796c22ab1957565ea9720cabc09cb9eb
tomaadriandaniel <toma_adrian27@yahoo.com>
2025-09-13 18:36:50 +0300

    cors again

--- src/main/java/ro/ctrln/config/CorsConfig.java
+++ src/main/java/ro/ctrln/config/CorsConfig.java
@@ -1,24 +1,19 @@
 package ro.ctrln.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
- @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // ✅ înlocuiește allowedOrigins
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true) // dacă vei trimite cookie/token
-                        .maxAge(3600);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173", "https://bittnetshop-frontend.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
