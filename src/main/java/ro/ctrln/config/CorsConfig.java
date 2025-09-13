e1575923a6e1bcf48a8c68da5004070df6b00906
tomaadriandaniel <toma_adrian27@yahoo.com>
2025-09-13 17:19:42 +0300

    cors config added

---
+++ src/main/java/ro/ctrln/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package ro.ctrln.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:5173", 
+                            "https://bittnetshop-frontend.netlify.app" // pentru Netlify mai târziu
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
