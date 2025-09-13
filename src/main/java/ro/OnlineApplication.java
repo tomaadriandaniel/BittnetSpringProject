deece6d0ca0e941db7aa127cad94d479822360fb
tomaadriandaniel <toma_adrian27@yahoo.com>
2025-09-13 18:43:59 +0300

    cors corrected component scan

--- src/main/java/ro/OnlineApplication.java
+++ src/main/java/ro/OnlineApplication.java
@@ -2,13 +2,11 @@ package ro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan(basePackages = "ro.ctrln")
 public class OnlineApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(OnlineApplication.class, args);
     }
 }
